9dd2eee3aab62bb693739142127256760cae5ccc
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2023-04-08 01:36:05 -0400

    algorithm for determining where a user has moved a piece is finished

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -15,8 +15,19 @@ import wbishop from '../assets/wbishop.svg.png'
 
 
 const Board = () => {
-    const squaresRef = useRef<HTMLDivElement[]>([])
-    console.log(squaresRef)
+    const squaresRef = useRef<any>([])
+
+    function checkPieceLocation(piece: React.MouseEvent<HTMLDivElement, MouseEvent>, data: any, e: any) {
+        let pieceLocation = data.target.getBoundingClientRect()
+        squaresRef.current.forEach((square:any) => {
+            if(pieceLocation.bottom <= square.location.bottom && pieceLocation.top >= square.location.top && pieceLocation.left >= square.location.left && pieceLocation.right <= square.location.right) {
+              console.log("pieceLocation--->", pieceLocation)
+              console.log("square.location--->", square.location)
+              console.log('square element ---->' , square.element)
+              console.log("square --->", square.element.id)
+                }
+                })
+    }    
     return (
     <>
     <div className="flex w-screen h-screen items-center justify-center">
@@ -27,7 +38,7 @@ const Board = () => {
                 return Array(4).fill(0).map((_, index2) => 
                     <>
                     <div 
-                    ref={el => el != null && squaresRef.current.push(el)}
+                    ref={el => el != null && squaresRef.current.push({"element": el.id, "location": el.getBoundingClientRect()})}
                     id={`white-${index2 == 0? "A":
                         index2 == 1? "C":
                         index2 == 2? "E":
@@ -44,8 +55,8 @@ const Board = () => {
                         </div>
                         {Math.abs(index1-8) == 2 &&
                         <Draggable 
-                        onStart={(e, data) => console.log(e?.target?.attributes[0].nodeValue)} 
-                        onStop={(e, data) => console.log(e?.target?.attributes[0].nodeValue)}>
+                        onStart={(e, data) => {}} 
+                        onStop={(e, data) => checkPieceLocation(e?.target?.attributes[0].nodeValue, e, data)}>
                         <img 
                         src={wpawn}
                         className="w-[5rem] mr-[19.5px]" />
@@ -82,11 +93,11 @@ const Board = () => {
                         </div>
                     
                     <div 
-                    ref={el => squaresRef.current.push(el)}
-                    id={`green-${index2 == 0? "A":
-                        index2 == 1? "C":
-                        index2 == 2? "E":
-                        index2 == 3? "G":
+                    ref={el => el != null && squaresRef.current.push({"element": el, "location": el.getBoundingClientRect()})}
+                    id={`green-${index2 == 0? "B":
+                        index2 == 1? "D":
+                        index2 == 2? "F":
+                        index2 == 3? "H":
                         null}-${Math.abs(index1-8)}`}  
                         className="h-28 w-28 bg-green-500 border-[1px] flex items-center border-gray-300 font-bold">
                     <div className='mb-auto text-sm'>
@@ -138,7 +149,7 @@ const Board = () => {
                 return Array(4).fill(0).map((_, index3) =>
                     <>
                     <div 
-                    ref={el => squaresRef.current.push(el)}
+                    ref={el => el != null && squaresRef.current.push({"element": el, "location": el.getBoundingClientRect()})}
                     id={`white-${index3 == 0? "A":
                         index3 == 1? "C":
                         index3 == 2? "E":
@@ -190,11 +201,11 @@ const Board = () => {
                     }
                     </div>
                     <div 
-                    ref={el => squaresRef.current.push(el)}
-                    id={`white-${index3 == 0? "A":
-                        index3 == 1? "C":
-                        index3 == 2? "E":
-                        index3 == 3? "G":
+                    ref={el => el != null && squaresRef.current.push({"element": el, "location": el.getBoundingClientRect()})}
+                    id={`white-${index3 == 0? "B":
+                        index3 == 1? "D":
+                        index3 == 2? "F":
+                        index3 == 3? "H":
                         null}-${Math.abs(index1-8)}`}  className="h-28 w-28 bg-white border-[1px] flex items-center border-gray-300 font-bold">
                     <div className='mb-auto text-sm'>
                     {Math.abs(index1-8)} 
